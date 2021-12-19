70a1b35c21bd797d2023347b3a372c83d0e3fd95
Md Faisal Ahmed <md.faisal3@northsouth.edu>
2021-12-19 19:51:07 +0600

    login page updated

--- alphaapp/urls.py
+++ alphaapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import Signup, SellerSignupVIew, CustomerSignUpView
+
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('main/', views.main, name='main'),
-    path('login/', views.login, name='login')   
+    path('login/', views.login, name='login'),
+    path('signup', views.Signup, name='signup'),
+    path('alphaapp/signup/customer/', CustomerSignUpView.as_view(), name='customer_signup'),
+    path('alphaapp/signup/seller/',   SellerSignupVIew.as_view(), name='seller_signup'),
 ]
--- alphaapp/views.py
+++ alphaapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.views.generic import CreateView
 from django.http import HttpResponse
+from .forms import CustomerSignupForm, SellerSignUpForm
+from .models import User, customer, seller
 
 # Create your views here.
 def home(request):
@@ -10,4 +13,36 @@ def main(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def Signup(request):
+    return render(request, 'signup.html')
+
+
+class CustomerSignUpView(CreateView):
+    model = User
+    form_class = CustomerSignupForm
+    template_name = 'signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'guser'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class SellerSignupVIew(CreateView):
+    model = User
+    form_class = SellerSignUpForm
+    template_name = 'signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'trainmaster'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        #login(self.request, user)
+        return redirect('home')
    
